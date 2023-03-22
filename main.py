ab0f79f8ea668db24d5e18aa5f95ae3cce7889c7
Carleson Kalebe <carlesonkalebe2016@gmail.com>
2023-03-22 15:29:18 -0400

    teste

--- main.py
+++ main.py
@@ -10,6 +10,3 @@ print(fila_teste2.chama_cliente(10))
 print(fila_teste2.chama_cliente(20))
 print(fila_teste2.chama_cliente(15))
 print(fila_teste2.estatistica('11/01/1998', 190  , 'detail'))
-
-
-
