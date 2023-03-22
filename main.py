631fa6c7a827502145e33e860901aabfbe452e13
Carleson Kalebe <carlesonkalebe2016@gmail.com>
2023-03-22 14:56:41 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from fila import FilaNormal
+from fila_prioritaria import FilaPrioritaria
+
+fila_teste2 = FilaPrioritaria()
+fila_teste2.atualiza_fila()
+fila_teste2.atualiza_fila()
+fila_teste2.atualiza_fila()
+fila_teste2.atualiza_fila()
+print(fila_teste2.chama_cliente(10))
+print(fila_teste2.chama_cliente(20))
+print(fila_teste2.chama_cliente(15))
+print(fila_teste2.estatistica('10/01/1998', 180, 'detail'))
+
